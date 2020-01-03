e72ede7074768b0b17497c075679598fd502a70d
KingCobra2018 <kcobra557@gmail.com>
2020-01-03 18:00:30 +0000

    added render files for each components with new app.js config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from './components/testimonials/testimonials';
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// eslint-disable-next-line no-undef
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <About/>
+                <Resume/>
+                <Portfolio/>
+                <Testimonials/>
+                <ContactUs/>
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
